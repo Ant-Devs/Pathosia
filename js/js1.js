f5a4ccc51fac65270a561c63c894346701b3c37f
Ant Developers <55980467+Ant-Developers@users.noreply.github.com>
2020-10-12 13:36:04 +0700

    Updated flag and webpage

---
+++ js/js1.js
@@ -0,0 +1,12 @@
+function sendMsg(msg)
+{
+    alert(msg);
+}
+function changeColor(id, color)
+{
+    document.getElementById(id).style.backgroundColor = color;
+}
+function openWeb(web)
+{
+    window.open(web);
+}
